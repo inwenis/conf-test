f54258ea110545f117574b8110c6ef1fc1d4d015
Filip Kucharczyk <fil.kucharczyk@gmail.com>
2024-11-20 14:10:01 +0100

    initial

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateApplicationBuilder();
+Console.WriteLine(builder.Environment.EnvironmentName);
+var host = builder.Build();
+var confService = host.Services.GetService(typeof(IConfiguration)) as IConfiguration;
+var url = confService.GetValue<string>("UrlOfThatImportantService");
+Console.WriteLine($"UrlOfThatImportantService: {url}");
